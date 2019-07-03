36d3322152b0bb8e9423d1bf45b49e477087229d
Matt Burger <matt.burger1@outlook.com>
2019-07-03 12:56:04 -0700

    code complete tests needed

--- src/main/java/com/mjbmjb/cf/taskmaster/taskmaster/TaskMasterRepository.java
+++ src/main/java/com/mjbmjb/cf/taskmaster/taskmaster/TaskMasterRepository.java
@@ -5,10 +5,9 @@ import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @EnableScan
 public interface TaskMasterRepository extends CrudRepository<TaskMaster, String> {
 
-    Optional<TaskMaster> findById(UUID id);
+    Optional<TaskMaster> findById(String id);
 }
